c81e1b8ca22fe89af5249c4ab8cb50cd038bfaba
daiki kuroiwa <daikikuroiwa@daikinoMacBook-Air.local>
2024-08-02 08:04:16 +0900

    質問項目追加

--- src/pages/Research/MyPage/index.jsx
+++ src/pages/Research/MyPage/index.jsx
@@ -79,8 +79,8 @@ const Home = () => {
     }
     return (
         <div className={ styles.background_home }>
-            <div style ={{position: 'absolute', top: 20, width: '60%', textAlign: 'center', background: 'white'}}>
-                <p style={{ fontSize: '60px', fontWeight: 'bold', color: '#ea9917'}}>
+            <div style ={{ position: 'absolute', top: 20, width: '75%', textAlign: 'center', backgroundColor: '#ffffff'}}>
+                <p style={{ fontSize: '60px', fontWeight: 'bold', color: '#ea9917' }}>
                     AIと一緒に自由研究テーマを決めよう!
                 </p>
             </div>
@@ -89,21 +89,48 @@ const Home = () => {
             onClick={handleLogout}
             className={styles.button_LogOut}>ログアウト
             </button>
-
-
-
-            <p style ={{ position: 'absolute', bottom: '200px', }}>
-                いくつかの質問に答えてみよう！
+            <div style={{ position: 'absolute', bottom: '200px', width: '100%', textAlign: 'center', backgroundColor: '#f3960b'}}>
+            <p style ={{ fontSize: '30px', color: 'black'}}>
+                いくつかの質問に答えてね！
             </p>
-        <label>
-          興味のある分野:
-          <input
-            type="text"
-            value={interests}
-            onChange={(e) => setInterests(e.target.value)}
-            placeholder="例: 科学, 歴史, 自然, アート"
-          />
-        </label>
+            </div>
+            <label>
+                学校で好きな科目は？:
+                <input
+                type="text"
+                value={interests}
+                onChange={(e) => setInterests(e.target.value)}
+                // className={styles.question_input}
+                />
+            </label>
+            <label style={{ fontSize: '20px'}}>
+                理科の授業で面白かった実験は？
+                <input
+                type="text"
+                value={interests}
+                onChange={(e) => setInterests(e.target.value)}
+                placeholder="例: 水の性質、燃焼、光の反射と屈折、バネと力、昆虫の観察、天気の観察"
+                className={styles.question_input}
+                />
+            </label>
+            <label>
+                どのくらいの期間で終わらせたい？:
+                <input
+                type="text"
+                value={interests}
+                onChange={(e) => setInterests(e.target.value)}
+                placeholder="例: 科学, 歴史, 自然, アート"
+                />
+            </label>
+            <label>
+                家にあるもので何か使ってみたいものはある？:
+                <input
+                type="text"
+                value={interests}
+                onChange={(e) => setInterests(e.target.value)}
+                placeholder="例: 科学, 歴史, 自然, アート"
+                />
+            </label>
         </div>
     )
 };
