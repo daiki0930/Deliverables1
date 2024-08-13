70d56c811f27293be8ffb44e573cbde809b1e806
daiki kuroiwa <daikikuroiwa@daikinoMacBook-Air.local>
2024-08-14 08:01:39 +0900

    コンフリクト修正したと思う

--- src/pages/api/firebase/firebaseAdmin.js
+++ src/pages/api/firebase/firebaseAdmin.js
@@ -1,10 +1,14 @@
 // import admin from 'firebase-admin';
-// import serviceAccount from './serviceAccount.json';
+// // import serviceAccount from './serviceAccount.json';
 
 
 // if (!admin.apps.length) {
 //     admin.initializeApp({
-//         credential: admin.credential.cert(serviceAccount),
+//         credential: admin.credential.cert({
+//             projectId: process.env.GOOGLE_CLOUD_PROJECT_ID,
+//             privateKey: process.env.GOOGLE_CLOUD_PRIVATE_KEY.replace(/\\n/g, '\n'),
+//             clientEmail: process.env.GOOGLE_CLOUD_CLIENT_EMAIL,
+//         }),
 //     });
 // }
 
