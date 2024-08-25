1530406a3312b09f132f882e703cb476c99a4dbc
daiki kuroiwa <daikikuroiwa@daikinoMacBook-Air.local>
2024-08-25 19:15:45 +0900

    API作成した、あとはクレジット

--- src/pages/api/generate-theme.js
+++ src/pages/api/generate-theme.js
@@ -1,16 +1,18 @@
-import { Configuration, OpenAIApi } from 'openai';
+import OpenAI from 'openai';
 
-const configuration = new Configuration({
+const openai = new OpenAI({
     apiKey: process.env.OPEN_AI_API_KEY,
 });
 
-const openai = new OpenAIApi(configuration);
+// const openai = new OpenAIApi(configuration);
 
 export default async function handler(req, res) {
     if (req.method === 'POST') {
+        console.log('------届いているか--------', req.body)
         const { interests, interests1, interests2, interests3 } = req.body;
 
         try {
+            console.log('------届いているか1--------');
             const completion = await openai.chat.completions.create({
                 model: 'gpt-3.5-turbo',
                 messages: [
@@ -18,14 +20,23 @@ export default async function handler(req, res) {
                     { role: 'user', content: `Suggest a research theme for an elementary school student. Subjects: ${interests}. An interesting experiment: ${interests1}. Duration: ${interests2}. Available materials: ${interests3}`}
                 ]
             });
+            console.log('------届いているか2--------', completion)
 
+            try {
             const responseText = completion.data.choices[0].message.content.trim();
+            console.log('------届いているか3-------', responseText)
 
             const themeTitle = responseText.split('\n')[0];
+            console.log('------届いているか4-------', themeTitle)
 
             res.status(200).json({ theme: themeTitle });
+            } catch (ResponseError) {
+                console.error('応答処理のエラーです。', ResponseError)
+                res.status(500).json({ error: 'response error.'});
+            }
         } catch (error) {
-            res.status(500).json({ error: 'エラーです。'});
+            console.error('APIのエラーです。', error)
+            res.status(500).json({ error: 'API error.'})
         }
     } else {
         res.status(405).end();
